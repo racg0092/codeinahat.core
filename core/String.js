f431b5214b13b9f6349f6558aa1331cb0ef0b0d2
racg0092 <chapman.gomez.richard@gmail.com>
2020-02-24 15:15:54 -0500

    README updated

---
+++ core/String.js
@@ -0,0 +1,10 @@
+module.exports = function() {
+    String.prototype.IsNullOrEmpty = function() {
+        return this === null || this.match(/^ *$/) !== null;
+    }
+
+    String.prototype.IsEmail = function() {
+        let expresssion = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+        return expresssion.test(this);
+    }
+}
