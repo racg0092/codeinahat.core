537acb14ca2848d7728e8beebe7fa5953f025818
racg0092 <chapman.gomez.richard@gmail.com>
2020-02-25 08:44:04 -0500

    README.MD updated

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const core = require('./core');
 
 
 const group = [
-    {name: "Tito",age: 10, role: "sys_admin"},
-    {name: "Pedro", age: 25, role: "sales_sup"},
-    {name: "Toney", age: 30, role: "coorp"},
-    {name: "Harry", age: 20, role: "coord"},
-    {name: "Tito",age: 10, role: "sys_admin"},
+    {name: "Tito",age: 10, role: "sys_admin", email: 'asdhkjhda'},
+    {name: "Pedro", age: 25, role: "sales_sup", email: 'pedro@gmail.com'},
+    {name: "Toney", age: 30, role: "coorp", email: 'tes@1111'},
+    {name: "Harry", age: 20, role: "coord", email: 'harry@yahoo.es'},
+    {name: "Tito",age: 10, role: "sys_admin", email: 'tio'},
 ]
 
 
@@ -17,6 +17,6 @@ const g = group.FirstOrDefault();
 
 const g2 = group.Distinct();
 
-
+console.log(group[4].email.IsEmail());
 console.log(g);
 console.log(g2);
